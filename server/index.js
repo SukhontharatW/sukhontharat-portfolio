423289049783ba357ef677f4057c4378802ec931
Sukhontharat Wetthaisong <77877109+SukhontharatW@users.noreply.github.com>
2024-08-07 18:16:44 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors");
 app.use(
   cors({
-    origin: "*",
+    origin: "https://sukhontharat-portfolio.onrender.com",
     credentials: true,
   })
 );
